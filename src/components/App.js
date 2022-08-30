40aff003c8c2aabe1e71fde967a743e5235b5eb0
Jasir Ali <jasirali407@gmail.com>
2022-08-30 21:49:39 +0530

    add UI, actions, reducers

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
-
+import {data} from '../data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
+// console.log('hii',data);
 
 
 function App() {
   return (
     <div className="App">
-     
+      <Navbar />      
+
+       <div className='main'>
+
+        <div className='tabs'>
+          <div className='tab'>Movies</div>
+          <div className='tab'>Favourites</div>
+        </div>
+          
+          <div className='list'>
+            {data.map(movie=>
+           (<MovieCard movie={movie} />))
+            }            
+          </div>
+
+        </div> 
+
        
+            
     </div>
   );
 }
