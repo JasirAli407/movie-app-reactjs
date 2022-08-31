f45d1b1b8dd102a5fed9a0d9da03cd8bf5a3ef85
Jasir Ali <jasirali407@gmail.com>
2022-08-31 20:14:04 +0530

    dispatch and subscribe to the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,51 @@
+import React from 'react';
 import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 // console.log('hii',data);
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />      
+class App extends React.Component {
+  componentDidMount(){
+    const {store} = this.props;
+     
+    store.subscribe(()=>{
+      console.log('updated')
+      //  console.log(this);
+      this.forceUpdate()
+    })
+    store.dispatch({
+      type: 'ADD_MOVIES',
+      movies: data
+    })
 
-       <div className='main'>
+    // console.log('state', store.getState());
 
-        <div className='tabs'>
-          <div className='tab'>Movies</div>
-          <div className='tab'>Favourites</div>
-        </div>
-          
-          <div className='list'>
-            {data.map(movie=>
-           (<MovieCard movie={movie} />))
-            }            
-          </div>
+  }
+  render(){
+    console.log('render');
+    const movies = this.props.store.getState();
+      return (
+        <div className="App">
+          <Navbar />      
 
-        </div> 
+          <div className='main'>
 
-       
-            
-    </div>
-  );
+            <div className='tabs'>
+              <div className='tab'>Movies</div>
+              <div className='tab'>Favourites</div>
+            </div>
+              
+              <div className='list'>
+                {movies.map((movie, index)=>
+              (<MovieCard movie={movie} key={`movies-${index}`} />))
+                }            
+              </div>
+
+            </div>              
+         </div>
+    );
+ }
 }
 
 export default App;
